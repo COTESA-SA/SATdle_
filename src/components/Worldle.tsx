8e95e74766810000df3d4c49ccb80744e3c90515
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:52:44 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -6,7 +6,7 @@ export function Worldle() {
       SATd<span className="text-green-600">L</span>E
     </span>
     <div className="space-y-3 text-justify border-b-2 border-gray-200 pb-3 mb-3">
-      <div>Guess the <Worldle /> in 6 guesses.</div>
+      <div>Developed by Alejandro Redondo and Javier Becerra but strongly based on @teuteuf and @aommm. Thank you, guys!.</div>
     </div>
   );
 }
