562532d70b8c8c4ce8773c5942da13c2926310cc
Javier Becerra <javibecerra.10@gmail.com>
2022-06-02 00:24:55 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
-      SATd<span className="text-green-600">L</span>E·Forked·by·Alejandro·Redondo·and·Javier·Becerra·thus·strongly·based·on·teuteuf·and·aommm·Thank·you·guys
+      SATd<span className="text-green-600">L</span>E·Forked·by·Alejandro·Redondo·and·Javier·Becerra·
+      thus·strongly·based·on·teuteuf·and·aommm·Thank·you·guys
     </span>
   );
 }
