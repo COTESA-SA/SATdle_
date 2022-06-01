8a0341501d9fdb7c0ccf08427d55178e7b3baa10
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:38:34 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
+      A guessing game by using free satellite imagery from different sensors and resolutions. Play it here: https://javierbecerra.github.io/SATdle_/
+      Forked by Alejandro Redondo and Javier Becerra and, thus, strongly based on @teuteuf and @aommm. Thank you guys!
       SATd<span className="text-green-600">L</span>E
     </span>
   );
