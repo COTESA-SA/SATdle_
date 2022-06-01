0c2340806ebcbeae291f05dbc7d17e59bcaaa1eb
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:51:36 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -7,8 +7,6 @@ export function Worldle() {
     </span>
     <div className="space-y-3 text-justify border-b-2 border-gray-200 pb-3 mb-3">
       <div>Guess the <Worldle /> in 6 guesses.</div>
-      <div>Each guess must be a valid municipality.</div>
-      <div>After each guess, you will have the distance, the direction and the proximity from your guess and the target municipality.</div>
     </div>
   );
 }
