12c13c06067217c567840531488087e94cfd8765
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:56:04 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -5,8 +5,6 @@ export function Worldle() {
     <span className="font-bold">
       SATd<span className="text-green-600">L</span>E
     </span>
-    <div className="space-y-3 text-justify border-b-2 border-gray-200 pb-3 mb-3">
-      <div>Forked by Alejandro Redondo and Javier Becerra thus strongly based on @teuteuf and @aommm. Thank you, guys!.</div>
-    </div>
+    <div>Forked by Alejandro Redondo and Javier Becerra thus strongly based on @teuteuf and @aommm. Thank you, guys!.</div>
   );
 }
