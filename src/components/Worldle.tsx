ca81587360f8d8b27bb9591b774619bddfa5f00c
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:51:13 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -6,14 +6,9 @@ export function Worldle() {
       SATd<span className="text-green-600">L</span>E
     </span>
     <div className="space-y-3 text-justify border-b-2 border-gray-200 pb-3 mb-3">
-      <div>
-        Guess the <Worldle /> in 6 guesses.
-      </div>
+      <div>Guess the <Worldle /> in 6 guesses.</div>
       <div>Each guess must be a valid municipality.</div>
-      <div>
-        After each guess, you will have the distance, the direction and the
-        proximity from your guess and the target municipality.
-      </div>
+      <div>After each guess, you will have the distance, the direction and the proximity from your guess and the target municipality.</div>
     </div>
   );
 }
