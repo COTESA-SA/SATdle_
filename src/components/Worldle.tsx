5ac76d4ca75e7f7bc9732b210b94f6b696897ace
Javier Becerra <javibecerra.10@gmail.com>
2022-06-01 23:50:02 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -5,9 +5,15 @@ export function Worldle() {
     <span className="font-bold">
       SATd<span className="text-green-600">L</span>E
     </span>
-    <div>
-      A guessing game by using free satellite imagery from different sensors and resolutions. Play it here: https://javierbecerra.github.io/SATdle_/
-      Forked by Alejandro Redondo and Javier Becerra and, thus, strongly based on @teuteuf and @aommm. Thank you guys!
+    <div className="space-y-3 text-justify border-b-2 border-gray-200 pb-3 mb-3">
+      <div>
+        Guess the <Worldle /> in 6 guesses.
+      </div>
+      <div>Each guess must be a valid municipality.</div>
+      <div>
+        After each guess, you will have the distance, the direction and the
+        proximity from your guess and the target municipality.
+      </div>
     </div>
   );
 }
