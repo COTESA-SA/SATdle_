935fb575a05b52ae7f2a5b9b9a91ee38024520f8
Javier Becerra <javibecerra.10@gmail.com>
2022-06-02 12:43:29 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
-      SATd<span className="text-green-600">L</span>E·FROM·AR·JB
+      SATd<span className="text-green-600">L</span>E·
+      FROM·AR·JB
     </span>
   );
 }
