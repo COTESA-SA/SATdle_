f8ea6a8fd3e7da8d3ca6e16c681eac9a9149a78e
Alejandro Redondo <aredondosy@alumnos.unex.es>
2022-06-27 09:02:10 +0200

    Update Share.tsx

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -60,7 +60,7 @@ export function Share({
       })
       .join("\n");
 
-    return [title, guessString, "https://aredondosy.github.io/SATdle_"].join("\n");
+    return [title, guessString, "https://aredondosy.github.io/SATdle_/"].join("\n");
   }, [dayString, guesses, hideImageMode, rotationMode, theme]);
 
   return (
