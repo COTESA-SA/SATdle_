f88be8daa7de3f19b3692fac40756b5e974e71fc
Alejandro Redondo <aredondosy@alumnos.unex.es>
2022-06-27 09:48:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,7 @@ function App({
               text="❤️"
               className="flex items-center justify-center mr-1"
             />{" "}
-            <Worldle /> -{" "}
+            <Worldle />{" "}-{" "}
             {country && supportLink[country.code] != null ? (
               <a
                 className="underline pl-1"
@@ -142,7 +142,7 @@ function App({
                 <div className="w-max">{t(`support.${country.code}`)}</div>
               </a>
             ) : (
-              " Me too! "
+              "{" "}Me too!{" "}"
             )}
             <a title="COTESA" href="https://cotesa.grupotecopy.es/">
               <img src="logo192.png" alt="COTESA logo" width="60" height="60" />
