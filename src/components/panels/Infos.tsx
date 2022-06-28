e1755885ba550717ea5227a97a4ec46bbbc3da45
Alejandro Redondo <alejandroredondo@grupotecopy.es>
2022-06-28 16:20:52 +0200

    Update Infos.tsx

--- src/components/panels/Infos.tsx
+++ src/components/panels/Infos.tsx
@@ -129,7 +129,7 @@ export function Infos({ isOpen, close, settingsData }: InfosProps) {
           >
             COTESA
           </a>
-          ) - <a href="alejandroredondo@grupotecopy.es">Contact us</a>
+          ) - <a href="mailto:alejandroredondo@grupotecopy.es">Contact us</a>
         </div>
       </div>
     </Panel>
