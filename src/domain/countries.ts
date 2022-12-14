73020687467057a793f0c9cc18fa87491f0a4199
Alejandro Redondo <alejandroredondo@grupotecopy.es>
2022-12-14 16:15:15 +0100

    Update countries.ts

--- src/domain/countries.ts
+++ src/domain/countries.ts
@@ -28,7 +28,6 @@ const countryCodesWithImage = [
   "Biarritz",
   "Braganca",
   "Bregenz", 
-  "Brod", x
   "Burgos",
   "Cadiz",
   "Cartagena",
@@ -141,18 +140,6 @@ export const countries: Country[] = [
     latitude: 45.734955,
   },
   {
-    name: "Appenzell",
-    code: "Appenzell",
-    longitude: 9.408623,
-    latitude: 47.3314175,
-  },
-  {
-    name: "Arlon",
-    code: "Arlon",
-    longitude: 5.816699,
-    latitude: 49.6833,
-  },
-  {
     name: "Barcelona",
     code: "Barcelona",
     longitude: 2.15899991,
@@ -219,18 +206,6 @@ export const countries: Country[] = [
     latitude: 38.9950921,
   },
   {
-    name: "Alexandria",
-    code: "Alexandria",
-    longitude: 25.333,
-    latitude: 43.967,
-  },
-  {
-    name: "Alexandroupoli",
-    code: "Alexandroupoli",
-    longitude: 25.8753275,
-    latitude: 40.845692,
-  },
-  {
     name: "Algeciras",
     code: "Algeciras",
     longitude: -5.4473991,
@@ -241,18 +216,6 @@ export const countries: Country[] = [
     code: "Almeria",
     longitude: -2.4628135,
     latitude: 36.8414197,
-  },
-  {
-    name: "Arendal",
-    code: "Arendal",
-    longitude: 8.94694911492874,
-    latitude: 58.42673005,
-  },
-  {
-    name: "Arnhem",
-    code: "Arnhem",
-    longitude: 5.9108573,
-    latitude: 51.984257,
   },
   {
     name: "Assen",
@@ -279,12 +242,6 @@ export const countries: Country[] = [
     latitude: 38.0154479,
   },
   {
-    name: "Bellinzona",
-    code: "Bellinzona",
-    longitude: 9.0205888,
-    latitude: 46.1920538,
-  },
-  {
     name: "Besancon",
     code: "Besancon",
     longitude: 6.0243622,
@@ -303,22 +260,10 @@ export const countries: Country[] = [
     latitude: 41.5084468,
   },
   {
-    name: "Branco",
-    code: "Branco",
-    longitude: -7.49189,
-    latitude: 39.8239,
-  },
-  {
     name: "Bregenz",
     code: "Bregenz",
     longitude: 9.7472924,
     latitude: 47.5025779,
-  },
-  {
-    name: "Brod",
-    code: "Brod",
-    longitude: 18.0121385,
-    latitude: 45.1665803,
   },
   {
     name: "Burgos",
@@ -333,40 +278,16 @@ export const countries: Country[] = [
     latitude: 36.5297438,
   },
   {
-    name: "Campobasso",
-    code: "Campobasso",
-    longitude: 14.826226697820019,
-    latitude: 41.717264799999995,
-  },
-  {
     name: "Cartagena",
     code: "Cartagena",
     longitude: -0.9841152,
     latitude: 37.6019353,
   },
   {
-    name: "Cherkasy",
-    code: "Cherkasy",
-    longitude: 31.2271744,
-    latitude: 49.1460165,
-  },
-  {
-    name: "Chernivtsi",
-    code: "Chernivtsi",
-    longitude: 25.9376532,
-    latitude: 48.2864702,
-  },
-  {
     name: "Chernobyl",
     code: "Chernobyl",
     longitude: 30.2195609,
     latitude: 51.2705477,
-  },
-  {
-    name: "Chur",
-    code: "Chur",
-    longitude: 9.5264904,
-    latitude: 46.854747,
   },
   {
     name: "Coimbra",
@@ -393,52 +314,10 @@ export const countries: Country[] = [
     latitude: 47.531399,
   },
   {
-    name: "Deva",
-    code: "Deva",
-    longitude: 22.9008611,
-    latitude: 45.8817402,
-  },
-  {
-    name: "Diekirch",
-    code: "Diekirch",
-    longitude: 6.1600549,
-    latitude: 49.8690898,
-  },
-  {
-    name: "Dubasari",
-    code: "Dubasari",
-    longitude: 29.1493655,
-    latitude: 47.2687548,
-  },
-  {
-    name: "Eisenstadt",
-    code: "Eisenstadt",
-    longitude: 16.536215864955643,
-    latitude: 47.83875775,
-  },
-  {
-    name: "Elblag",
-    code: "Elblag",
-    longitude: 19.4044583,
-    latitude: 54.1558708,
-  },
-  {
-    name: "Erfurt",
-    code: "Erfurt",
-    longitude: 11.0287364,
-    latitude: 50.9777974,
-  },
-  {
     name: "Evora",
     code: "Evora",
     longitude: -7.9092808,
     latitude: 38.5707742,
-  },
-  {
-    name: "Falun",
-    code: "Falun",
-    longitude: 15.6323059,
-    latitude: 60.6070068,
   },
   {
     name: "Faro",
@@ -447,22 +326,10 @@ export const countries: Country[] = [
     latitude: 37.0162727,
   },
   {
-    name: "Frauenfeld",
-    code: "Frauenfeld",
-    longitude: 8.8963347,
-    latitude: 47.5561915,
-  },
-  {
     name: "Fribourg",
     code: "Fribourg",
     longitude: 7.1027113,
     latitude: 46.6789116,
-  },
-  {
-    name: "Gavle",
-    code: "Gavle",
-    longitude: 17.1467019,
-    latitude: 60.6750132,
   },
   {
     name: "Gijon",
@@ -471,70 +338,10 @@ export const countries: Country[] = [
     latitude: 43.5449422,
   },
   {
-    name: "Glarus",
-    code: "Glarus",
-    longitude: 9.108812,
-    latitude: 46.9796562,
-  },
-  {
-    name: "Grevenmacher",
-    code: "Grevenmacher",
-    longitude: 6.4430979,
-    latitude: 49.679313,
-  },
-  {
     name: "Guadalajara",
     code: "Guadalajara",
     longitude: -3.16649,
     latitude: 40.6298,
-  },
-  {
-    name: "Guarda",
-    code: "Guarda",
-    longitude: -7.195139236071309,
-    latitude: 40.7046066,
-  },
-  {
-    name: "Haapsalu",
-    code: "Haapsalu",
-    longitude: 23.5414798,
-    latitude: 58.9399019,
-  },
-  {
-    name: "Halmstad",
-    code: "Halmstad",
-    longitude: 12.8574827,
-    latitude: 56.6739826,
-  },
-  {
-    name: "Hameenlinna",
-    code: "Hameenlinna",
-    longitude: 24.46654,
-    latitude: 60.9948584,
-  },
-  {
-    name: "Hania",
-    code: "Hania",
-    longitude: 24.0191544,
-    latitude: 35.5120831,
-  },
-  {
-    name: "Harnosand",
-    code: "Harnosand",
-    longitude: 17.9379914,
-    latitude: 62.6323084,
-  },
-  {
-    name: "Herisau",
-    code: "Herisau",
-    longitude: 9.2787571,
-    latitude: 47.38555,
-  },
-  {
-    name: "Hillerod",
-    code: "Hillerod",
-    longitude: 12.3063909,
-    latitude: 55.9285735,
   },
   {
     name: "Huelva",
@@ -547,48 +354,6 @@ export const countries: Country[] = [
     code: "Jaen",
     longitude: -3.4920556120372,
     latitude: 37.9557275,
-  },
-  {
-    name: "Kalmar",
-    code: "Kalmar",
-    longitude: 16.575243899947836,
-    latitude: 57.02784235,
-  },
-  {
-    name: "Karlovac",
-    code: "Karlovac",
-    longitude: 15.5486303,
-    latitude: 45.4892522,
-  },
-  {
-    name: "Karlskrona",
-    code: "Karlskrona",
-    longitude: 15.5866422,
-    latitude: 56.1621073,
-  },
-  {
-    name: "Kirovohrad",
-    code: "Kirovohrad",
-    longitude: 31.7834817,
-    latitude: 48.3725226,
-  },
-  {
-    name: "Kristianstad",
-    code: "Kristianstad",
-    longitude: 14.1566859,
-    latitude: 56.0293778,
-  },
-  {
-    name: "Labem",
-    code: "Labem",
-    longitude: 14.0417,
-    latitude: 50.6583,
-  },
-  {
-    name: "Lamia",
-    code: "Lamia",
-    longitude: 22.4341691,
-    latitude: 38.9004584,
   },
   {
     name: "Leicester",
@@ -621,18 +386,6 @@ export const countries: Country[] = [
     latitude: 50.6450962,
   },
   {
-    name: "Liestal",
-    code: "Liestal",
-    longitude: 7.7347783,
-    latitude: 47.4839723,
-  },
-  {
-    name: "Linares",
-    code: "Linares",
-    longitude: -3.6343581,
-    latitude: 38.0928249,
-  },
-  {
     name: "Liverpool",
     code: "Liverpool",
     longitude: -2.99168,
@@ -649,12 +402,6 @@ export const countries: Country[] = [
     code: "London",
     longitude: -0.1276474,
     latitude: 51.5073219,
-  },
-  {
-    name: "Lorca",
-    code: "Lorca",
-    longitude: -1.6988053,
-    latitude: 37.6715679,
   },
   {
     name: "Luhansk",
@@ -711,12 +458,6 @@ export const countries: Country[] = [
     latitude: 51.4997302,
   },
   {
-    name: "Mikkeli",
-    code: "Mikkeli",
-    longitude: 27.2726569,
-    latitude: 61.6877956,
-  },
-  {
     name: "Milan",
     code: "Milan",
     longitude: 9.1896346,
@@ -741,12 +482,6 @@ export const countries: Country[] = [
     latitude: 50.4549568,
   },
   {
-    name: "Montana",
-    code: "Montana",
-    longitude: 43.4125,
-    latitude: 23.225,
-  },
-  {
     name: "Munich",
     code: "Munich",
     longitude: 11.5753822,
@@ -759,22 +494,10 @@ export const countries: Country[] = [
     latitude: 37.9923795,
   },
   {
-    name: "Nykoping",
-    code: "Nykoping",
-    longitude: 17.0120656,
-    latitude: 58.7545409,
-  },
-  {
     name: "Olomouc",
     code: "Olomouc",
     longitude: 17.251143,
     latitude: 49.5940567,
-  },
-  {
-    name: "Opole",
-    code: "Opole",
-    longitude: 17.9236408,
-    latitude: 50.6668184,
   },
   {
     name: "Oslo",
@@ -783,22 +506,10 @@ export const countries: Country[] = [
     latitude: 59.9133301,
   },
   {
-    name: "Perth",
-    code: "Perth",
-    longitude: 115.8605801,
-    latitude: -31.9558964,
-  },
-  {
     name: "Poitiers",
     code: "Poitiers",
     longitude: -0.0467273,
     latitude: 44.6545981,
-  },
-  {
-    name: "Portalegre",
-    code: "Portalegre",
-    longitude: -7.721513354015343,
-    latitude: 39.2076447,
   },
   {
     name: "Portimao",
@@ -825,12 +536,6 @@ export const countries: Country[] = [
     latitude: 50.0874654,
   },
   {
-    name: "Real",
-    code: "Real",
-    longitude: -7.74635,
-    latitude: 41.2959,
-  },
-  {
     name: "Rijeka",
     code: "Rijeka",
     longitude: 14.4412309,
@@ -849,28 +554,10 @@ export const countries: Country[] = [
     latitude: 43.9458623,
   },
   {
-    name: "Santarem",
-    code: "Santarem",
-    longitude: -8.6867081,
-    latitude: 39.2363637,
-  },
-  {
     name: "Sarajevo",
     code: "Sarajevo",
     longitude: 18.3866868,
     latitude: 43.8519774,
-  },
-  {
-    name: "Schaffhausen",
-    code: "Schaffhausen",
-    longitude: 8.634513,
-    latitude: 47.6960491,
-  },
-  {
-    name: "Schwerin",
-    code: "Schwerin",
-    longitude: 11.4148038,
-    latitude: 53.6288297,
   },
   {
     name: "San Sebastian",
@@ -885,12 +572,6 @@ export const countries: Country[] = [
     latitude: 37.3886303,
   },
   {
-    name: "Shumen",
-    code: "Shumen",
-    longitude: 26.9247362,
-    latitude: 43.2703797,
-  },
-  {
     name: "Sibenik",
     code: "Sibenik",
     longitude: 15.8961863,
@@ -901,18 +582,6 @@ export const countries: Country[] = [
     code: "Sion",
     longitude: 0.00822,
     latitude: 43.73903,
-  },
-  {
-    name: "Sliven",
-    code: "Sliven",
-    longitude: 26.3153528,
-    latitude: 42.6817633,
-  },
-  {
-    name: "Soro",
-    code: "Soro",
-    longitude: 86.73683730021526,
-    latitude: 21.2997306,
   },
   {
     name: "Stuttgart",
@@ -927,12 +596,6 @@ export const countries: Country[] = [
     latitude: 41.1172364,
   },
   {
-    name: "Tonsberg",
-    code: "Tonsberg",
-    longitude: 6.4302622,
-    latitude: 50.6370671,
-  },
-  {
     name: "Toulouse",
     code: "Toulouse",
     longitude: 1.4442469,
@@ -945,28 +608,10 @@ export const countries: Country[] = [
     latitude: 45.6496485,
   },
   {
-    name: "Tripoli",
-    code: "Tripoli",
-    longitude: 13.1777923,
-    latitude: 32.896672,
-  },
-  {
-    name: "Trnava",
-    code: "Trnava",
-    longitude: 17.5858175,
-    latitude: 48.3767652,
-  },
-  {
     name: "Utrecht",
     code: "Utrecht",
     longitude: 5.12768396945229,
     latitude: 52.080985600000005,
-  },
-  {
-    name: "Uzhgorod",
-    code: "Uzhgorod",
-    longitude: 22.3022569,
-    latitude: 48.6223732,
   },
   {
     name: "Valladolid",
@@ -975,40 +620,10 @@ export const countries: Country[] = [
     latitude: 41.6521328,
   },
   {
-    name: "Vannersborg",
-    code: "Vannersborg",
-    longitude: 16.7047073,
-    latitude: 58.5579658,
-  },
-  {
-    name: "Vejle",
-    code: "Vejle",
-    longitude: 9.5333238,
-    latitude: 55.7000057,
-  },
-  {
-    name: "Viborg",
-    code: "Viborg",
-    longitude: 9.4059354,
-    latitude: 56.4469128,
-  },
-  {
     name: "Vigo",
     code: "Vigo",
     longitude: -8.7247205,
     latitude: 42.2376602,
-  },
-  {
-    name: "Viljandi",
-    code: "Viljandi",
-    longitude: 25.5876563,
-    latitude: 58.3646073,
-  },
-  {
-    name: "Visby",
-    code: "Visby",
-    longitude: 18.297982,
-    latitude: 57.6379361,
   },
   {
     name: "Viseu",
@@ -1021,24 +636,6 @@ export const countries: Country[] = [
     code: "Vitoria-Gasteiz",
     longitude: -2.6724025,
     latitude: 42.8465088,
-  },
-  {
-    name: "Volos",
-    code: "Volos",
-    longitude: 22.9401864,
-    latitude: 39.3624833,
-  },
-  {
-    name: "Vossavangen",
-    code: "Vossavangen",
-    longitude: 6.4147445,
-    latitude: 60.6286944,
-  },
-  {
-    name: "Vratsa",
-    code: "Vratsa",
-    longitude: 23.716533924123755,
-    latitude: 43.399902499999996,
   },
   {
     name: "Warsaw",
