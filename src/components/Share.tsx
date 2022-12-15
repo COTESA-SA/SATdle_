a08a539a624a6ef2f677e2d89b3f283adfbdba6b
Alejandro Redondo <alejandroredondo@grupotecopy.es>
2022-12-15 12:42:43 +0100

    Update Share.tsx

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -60,9 +60,7 @@ export function Share({
       })
       .join("\n");
 
-    return [title, guessString, "https://cotesa.com.es/satdle/"].join(
-      "\n"
-    );
+    return [title, guessString, "https://cotesa.com.es/satdle/"].join("\n");
   }, [dayString, guesses, hideImageMode, rotationMode, theme]);
 
   return (
